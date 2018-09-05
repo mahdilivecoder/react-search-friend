711038a8a145598687d96bcd1c8f55f1bcd065c2
livecoder <m.dev76@yahoo.com>
2018-09-05 08:36:19 +0430

    first commit

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Brad',
+    username: 'Brad',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Livecoder',
+    username: 'Livecoder',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Mahdi',
+    username: 'Mahdi',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Abolfazl',
+    username: 'Abolfazl',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'FanRobo',
+    username: 'FanRobo',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Abas',
+    username: 'Abas',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kratus',
+    username: 'Kratus',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Yargs',
+    username: 'Yargs',
+    email: 'Sherwood@rosamond.me'
+  },
+
